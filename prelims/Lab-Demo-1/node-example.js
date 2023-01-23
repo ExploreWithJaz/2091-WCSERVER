ae5d0fbbbc4f3691b9694291ee0e7ba88916f67b
ExploreWithJaz <51773574+ExploreWithJaz@users.noreply.github.com>
2023-01-23 12:17:00 +0800

    Add files via upload
    Initial Prelims Files

---
+++ prelims/Lab-Demo-1/node-example.js
@@ -0,0 +1,13 @@
+//Name: Jazper Garcia
+//Module: 3
+//Week 2 and 3: Basic of NodeJS
+
+//First program
+console.log("Welcome to WCSERVER!!");
+
+//Creating my first function
+function Display(x) {
+    console.log(x);
+}
+
+Display(200);
