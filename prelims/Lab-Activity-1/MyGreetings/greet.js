238b96e7638628eb9c7b169059d52ef9d831ce12
ExploreWithJaz <51773574+ExploreWithJaz@users.noreply.github.com>
2023-01-23 18:35:41 +0800

    1 file modified

--- prelims/Lab-Activity-1/MyGreetings/greet.js
+++ prelims/Lab-Activity-1/MyGreetings/greet.js
@@ -1,7 +1,7 @@
-module.exports = function (firstPerson, SecondPerson){
-    this.firstPerson = firstPerson;
-    this.SecondPerson = SecondPerson;
-    this.greetThisPerson = function () {
-        return 'Hello ' + this.firstPerson + ' and ' + this.SecondPerson;
-    }
-}
+module.exports = function (firstPerson, SecondPerson) {
+  this.firstPerson = firstPerson;
+  this.SecondPerson = SecondPerson;
+  this.greetThisPerson = function () {
+    return 'Hello ' + this.firstPerson + '\nHello ' + this.SecondPerson;
+  };
+};
